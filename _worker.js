f7bae8be14402503e9f19f7e21acd27f86c2ed05
zjlinzhiyu <37919005+zjlinzhiyu@users.noreply.github.com>
2023-05-04 16:44:42 +0800

    Create _worker.js

---
+++ _worker.js
@@ -0,0 +1,17 @@
+export default {
+  async fetch(request, env) {
+    const url = new URL("https://api.openai.com/v1/chat/completions");
+    const myHeaders = {
+      "Authorization": "Bearer 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",
+      "Content-Type': 'application/json"
+    };
+      
+    // openai is already set all CORS heasders 
+    return fetch(url, {
+      headers: myHeaders,
+      method: request.method,
+      body: request.body,
+      redirect: 'follow'
+    });
+  }
+}
