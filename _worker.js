a33e0e0d6d190cd26184ef586308f5fdabf6d039
zjlinzhiyu <37919005+zjlinzhiyu@users.noreply.github.com>
2023-05-04 16:51:23 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -3,10 +3,10 @@ export default {
     const url = new URL("https://api.openai.com/v1/chat/completions");
     const myHeaders = {
       "Authorization": "Bearer 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",
-      "Content-Type”: “application/json"
+      "Content-Type": "application/json" 
     };
       
-    // openai is already set all CORS heasders 
+    // openai is already set all CORS headers 
     return fetch(url, {
       headers: myHeaders,
       method: request.method,
