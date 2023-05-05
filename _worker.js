79813cc97472c4edc2c0dd5dd5fc53cb5d3fb5d3
zjlinzhiyu <37919005+zjlinzhiyu@users.noreply.github.com>
2023-05-05 18:25:52 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
   async fetch(request, env) {
     const url = new URL("https://api.openai.com/v1/chat/completions");
     const myHeaders = {
-      "Authorization": "Bearer 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",
+      "Authorization": "Bearer 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",
       "Content-Type": "application/json" 
     };
       
